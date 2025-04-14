2022d408ec2f9677574128d53ab6e82d3a4c0d92
Ayush Singh <ayush.singh03042005@gmail.com>
2025-04-14 18:22:27 +0530

    No commit needed

---
+++ script.js
@@ -0,0 +1,56 @@
+let timer=60;
+let score=0;
+let hitrn=0;
+
+function incScore() {
+    score+=10;
+    document.querySelector("#incscore").textContent=score;
+}
+
+
+function makeBubble() {
+let clutter="";
+for(let i=0;i<168;i++) {
+    let rn=Math.floor(Math.random()*10);
+    clutter+= `<div class="bubble">${rn}</div>`
+}
+
+
+document.querySelector("#pbot").innerHTML=clutter;
+}
+
+function getNewHit() {
+    hitrn=Math.floor(Math.random()*10);
+    document.querySelector("#hitval").textContent=hitrn;
+}
+
+
+function runTimer() {
+    let timerint=setInterval(function(){
+        if(timer>0) {
+        timer--;
+        document.querySelector("#timerval").textContent=timer;
+        }
+        else{
+            clearInterval(timerint);
+            document.querySelector("#pbot").innerHTML=`<h1>Game Over!</h1>`;
+        }
+    },1000)
+   
+}
+
+document.querySelector("#pbot").
+addEventListener("click",function(dets){
+    let clickedNum=(Number(dets.target.textContent));
+    if(clickedNum===hitrn) {
+        incScore();
+        makeBubble();
+        getNewHit();        
+    }
+    
+})
+
+
+runTimer();
+makeBubble();
+getNewHit();
